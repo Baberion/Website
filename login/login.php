af2cf2f22469dbd854f45cf6c51c4907f610053b
Baberion <Baberion22@gmail.com>
2023-10-19 14:22:00 +0200

    progress

---
+++ login/login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+        <title></title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Orbitron&display=swap" >
+        <link rel="stylesheet" href="../style.css">
+    </head>
+    <body>
+        <div id="loginTitle">
+            <h3> Log In</h3>
+        </div>
+        <div class="container">
+            <div class="container">
+                <p> Username</p>
+                <input type="text" name="Username" aria-label="username">
+            </div>
+            <div class="container">
+                <p> Password</p>
+                <input type="password" name="Password" aria-label="password">
+            </div>
+        </div>
+    </body>
+
+</html>
+
+<?php
+
+?>
